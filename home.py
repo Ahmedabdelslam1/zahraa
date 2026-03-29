b5f66534a322eea48e245430f1e2fc35461d43bb
ahmed abdelsalam <125304750+Ahmedabdelslam1@users.noreply.github.com>
2026-03-29 13:15:28 +0200

    Update home.py

--- home.py
+++ home.py
@@ -52,4 +52,20 @@ def setup():
 
     return {"status": "ok"}
 
+@app.route("/setup", methods=["POST"])
+def setup():
+    data = request.json
+    db = db_connect()
+
+    db.execute("INSERT INTO companies (name) VALUES (?)", (data["company"],))
+    db.execute("INSERT INTO branches (name, phone) VALUES (?,?)",
+               (data["branch"], data["phone"]))
+
+    db.execute("INSERT INTO users (username, password, role) VALUES (?,?,?)",
+               (data["user"], data["pass"], "admin"))
+
+    db.commit()
+
+    return {"status": "ok"}
+
 app.run()
